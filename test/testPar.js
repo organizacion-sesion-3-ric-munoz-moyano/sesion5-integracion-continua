267c4d56ff68dc5a806cb2267e3bda2844654ea4
rmunozmoyano <131545271+rmunozmoyano@users.noreply.github.com>
2023-05-01 13:39:17 +0200

    Update testPar.js

--- test/testPar.js
+++ test/testPar.js
@@ -13,7 +13,7 @@ it('comprobar función esPar', function() {
   should.equal(operaciones.esPar(12), true);
   should.equal(operaciones.esPar(0), true);
   should.equal(operaciones.esPar(-15), false);
-  should.equal(operaciones.esPar(-4), true);
+  should.equal(operaciones.esPar(-6), true);
 });
 
 
